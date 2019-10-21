241368fcc919c6d7bc90f56583268cbabc736428
Andrew Matheny <androidmatheny@gmail.com>
2019-10-21 01:37:13 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+new_array = Array.new 
+i = 0
+while i < array.length
+  new_array.push(yield(array[i]))
+  i += 1
+  end
+new_array
+end
+
+def reduce(array, start_point = nil)
+  if start_point
+    total = start_point
+    i = 0 
+  else 
+    total = array[0]
+    i = 1 
+  end
+  while i < array.length 
+  total = yield(total, array[i])
+  i += 1 
+  end
+total
+end 
