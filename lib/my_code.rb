618775f8f0da3d2e637f333b10d8afc109c7d339
Taylor Coon <taylor.allan.coon@gmail.com>
2019-10-24 22:47:00 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,34 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i < array.length
+    new.push(yield(array[i]))
+    i += 1
+  end
+  return new
+end
+
+
+#map(source_array){|n| n * -1}
+
+#map(source_array){|n| n * 2}
+
+#map(source_array){|n| n * n}
+
+#map(source_array){|n| n = n}
+
+
+def reduce(array, sv = nil)
+  if sv
+    sum = sv
+    i = 0
+  else
+    sum= array[0]
+    i = 1
+  end
+while i < array.length
+  sum = (yield(sum, array[i]))
+ i += 1
+end
+return sum
+end
